c96c2d261051b33718eed8b745e9b2ac4c171271
Hirepan E <ginebrasyrel@gmail.com>
2022-05-26 18:00:38 -0500

    Inicializando proyecto

---
+++ weekly_mission_4/API_Fizzbuzz/lib/controllers/explorerController.js
@@ -0,0 +1,11 @@
+const explorerService = require('../../../Refactoring/lib/services/explorerSevice')
+const fizzbuzzService = require('../../../Refactoring/lib/services/fizzbuzzService')
+const reader = require('../../../Refactoring/lib/utils/reader')
+class explorerController{
+    
+
+}
+
+module.exports = explorerController
+
+console.log(explorerController.getExplorerByMission());
