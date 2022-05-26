beaa2c69eeae2c121f56c40fcb6be15ec478cada
Hirepan E <ginebrasyrel@gmail.com>
2022-05-26 13:14:25 -0500

    Agregando testing para buzz de fizzbuzService

--- weekly_mission_4/Refactoring/test/services/fizzbuzz.test.js
+++ weekly_mission_4/Refactoring/test/services/fizzbuzz.test.js
@@ -5,14 +5,16 @@ const filter3 = explorers.filter((explorer)=> explorer.score === 3)
         const filter3Modified = fizzbuzzService.applyValidationInExplorer(filter3[0])
         console.log(filter3Modified);
 describe('Testing para FizzbuzzService',()=>{
-    test('1. Crear Fizz si score: 3 === 0', ()=>{
-        const filter3 = explorers.filter((explorer)=> explorer.score === 3)
+    test('1. Crear propiedad objeto.trick = "Fizz" si, parameter%3 === 0', ()=>{
+        const filter3 = explorers.filter((explorer)=> explorer.score %3 === 0)
         const filter3Modified = fizzbuzzService.applyValidationInExplorer(filter3[0])
 
         expect(filter3Modified).toMatch(/FIZZ/)
     }),
-    test('2. ', ()=>{
-            
-            expect().toBe()
+    test('2. Crear propiedad objeto.trick = "Buzz" si, parameter%3 === 0', ()=>{
+        const filter5 = explorers.filter((explorer)=> explorer.score %5 === 0)
+        const filter3Modified = fizzbuzzService.applyValidationInExplorer(filter5[0])
+
+        expect(filter3Modified).toMatch(/BUZZ/)
         })
 })
