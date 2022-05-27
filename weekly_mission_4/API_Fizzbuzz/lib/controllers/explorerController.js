c65cca08de94ea866173286fd332d821bee4de9a
Hirepan E <ginebrasyrel@gmail.com>
2022-05-27 10:48:27 -0500

    Limpiando proyecto
    Require ya hace lo mismo que la funcion de reader.readJsonFile, no es necesario llamar un Json.parse para convertir un json a una lista de objetos de JS, require lo hace por sí mismo

--- weekly_mission_4/API_Fizzbuzz/lib/controllers/explorerController.js
+++ weekly_mission_4/API_Fizzbuzz/lib/controllers/explorerController.js
@@ -2,31 +2,20 @@ const explorerService = require('../../../Refactoring/lib/services/explorerSevic
 const fizzbuzzService = require('../../../Refactoring/lib/services/fizzbuzzService')
 const reader = require('../../../Refactoring/lib/utils/reader')
 
-// const explorersReaderRq = require('../../../Refactoring/explorers.json') // Sintaxis soportada por Jest para testing
-// let x = reader.readJsonFile('../../../Refactoring/explorers.json')
-// console.log(x);
-// console.log(explorersReaderRq);
-
 class explorerController{
     static getExplorerByMission(mission){
-        // const explorersReaderq = require('../../../Refactoring/explorers.json') // Sintaxis soportada por Jest para testing
-        // const explorersReaderq = reader.readJsonFile('../../../Refactoring/package.json')//Sintaxis soportada solo por node
+        const explorersReaderq = require('../../../Refactoring/explorers.json')
         const gettingExplorerByMission = explorerService.filterByMission(explorersReaderq, mission)
         return gettingExplorerByMission
     }
 
     static getExplorersUsernamesByMission(mission){
-        const explorersReaderq = reader.readJsonFile('../../../Refactoring/explorers.json')
-        // const explorersReaderq = require('../../../Refactoring/explorers.json')
+        const explorersReaderq = require('../../../Refactoring/explorers.json')
         return explorerService.getExplorersUsernamesByMission(explorersReaderq, mission)
     }
     static getExplorersAmonutByMission(mission){
-        const explorersReaderq = reader.readJsonFile('../../../Refactoring/explorers.json')
-        // const explorersReaderq = require('../../../Refactoring/explorers.json')
+        const explorersReaderq = require('../../../Refactoring/explorers.json')
         return explorerService.getAmountOfExplorersByMission(explorersReaderq, mission)
     }
 }
-// console.log(explorerController.getExplorerByMission('java'));
-// console.log(explorerController.getExplorersAmonutByMission('java'));
-// console.log(explorerController.getExplorersUsernamesByMission('java'));
 module.exports = explorerController
--- weekly_mission_4/API_Fizzbuzz/test/controllers/explorerController.test.js
+++ weekly_mission_4/API_Fizzbuzz/test/controllers/explorerController.test.js
@@ -2,16 +2,6 @@ const explorerService = require("../../../Refactoring/lib/services/explorerSevic
 const explorers = require('../../../Refactoring/explorers.json')
 const explorerController = require("../../lib/controllers/explorerController")
 
-// const x = require("../../../Refactoring/lib/utils/reader")
-// console.log(x.readJsonFile(('../../../Refactoring/explorers.json')));
-
-// const testGetExplorersUsernamesByMission = explorerController.getExplorersUsernamesByMission('node')
-// console.log(testGetExplorersUsernamesByMission);
-// const testgetExplorersAmonutByMission = explorerController.getExplorersAmonutByMission('node')
-// console.log(testgetExplorersAmonutByMission);
-// const testgetExplorerByMission = explorerController.getExplorerByMission('node')
-// console.log(testgetExplorerByMission);
-
 describe('Pruebas del Explorer Controller',()=>{
     test('1. Probando getExplorerByMission', ()=>{
         const testgetExplorerByMission = explorerController.getExplorerByMission('node')
