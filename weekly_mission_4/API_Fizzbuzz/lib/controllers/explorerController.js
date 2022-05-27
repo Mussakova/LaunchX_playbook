da236b388ee64942cce137d04017afaa658f04f7
Hirepan E <ginebrasyrel@gmail.com>
2022-05-27 12:09:43 -0500

    Limpieza 2.0

--- weekly_mission_4/API_Fizzbuzz/lib/controllers/explorerController.js
+++ weekly_mission_4/API_Fizzbuzz/lib/controllers/explorerController.js
@@ -1,20 +1,17 @@
 const explorerService = require('../../../Refactoring/lib/services/explorerSevice')
 const fizzbuzzService = require('../../../Refactoring/lib/services/fizzbuzzService')
-const reader = require('../../../Refactoring/lib/utils/reader')
+const explorersReaderq = require('../../../Refactoring/explorers.json')
 
 class explorerController{
     static getExplorerByMission(mission){
-        const explorersReaderq = require('../../../Refactoring/explorers.json')
         const gettingExplorerByMission = explorerService.filterByMission(explorersReaderq, mission)
         return gettingExplorerByMission
     }
 
     static getExplorersUsernamesByMission(mission){
-        const explorersReaderq = require('../../../Refactoring/explorers.json')
         return explorerService.getExplorersUsernamesByMission(explorersReaderq, mission)
     }
     static getExplorersAmonutByMission(mission){
-        const explorersReaderq = require('../../../Refactoring/explorers.json')
         return explorerService.getAmountOfExplorersByMission(explorersReaderq, mission)
     }
 }
