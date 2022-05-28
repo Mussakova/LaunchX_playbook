3dc03d8b329ea7f5fc611b889de6670a7d21fce3
Hirepan E <ginebrasyrel@gmail.com>
2022-05-27 19:21:12 -0500

    Conectando API con Backend

--- weekly_mission_4/API_Fizzbuzz/lib/server.js
+++ weekly_mission_4/API_Fizzbuzz/lib/server.js
@@ -11,4 +11,18 @@ app.get("/", (request, response) => {
     response.json({message: "FizzBuzz Api welcome!"});
 });
 
-
+app.get('/v1/explorers/:missionx', (request, response)=>{
+    const mission = request.params.missionx
+    const explorersInMission = explorerController.getExplorerByMission(mission)
+    response.json(explorersInMission)
+})
+app.get('/v1/explorers/amount/:amountx', (request, response)=>{
+    const amount = request.params.amountx
+    const explorersInMission = explorerController.getExplorersAmonutByMission(amount)
+    response.json(explorersInMission)
+})
+app.get('/v1/explorers/usernames/:usernamesx', (request, response)=>{
+    const usernames = request.params.usernamesx
+    const explorersInMission = explorerController.getExplorersUsernamesByMission(usernames)
+    response.json(explorersInMission)
+})
